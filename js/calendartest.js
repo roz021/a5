3788a9f0de4f47f77d644057defa85831de52cb6
Mayu Wilson <maw076@ucsd.edu>
2018-11-08 20:55:01 -0800

    CALENDAR

---
+++ js/calendartest.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+var complexData = [
+  {'dayindex': 1},
+  {'dayindex': 2},
+  {'dayindex': 3},
+  {'dayindex': 4},
+  {'dayindex': 5},
+  {'dayindex': 6},
+  {'dayindex': 7},
+  {'dayindex': 8},
+  {'dayindex': 9},
+  {'dayindex': 10},
+  {'dayindex': 11},
+  {'dayindex': 12},
+  {'dayindex': 13},
+  {'dayindex': 14},
+  {'dayindex': 15},
+  {'dayindex': 16},
+  {'dayindex': 17},
+  {'dayindex': 18},
+  {'dayindex': 19},
+  {'dayindex': 20},
+  {'dayindex': 21},
+  {'dayindex': 22},
+  {'dayindex': 23},
+  {'dayindex': 24},
+  {'dayindex': 25},
+  {'dayindex': 26},
+  {'dayindex': 27},
+  {'dayindex': 28},
+  {'dayindex': 29},
+  {'dayindex': 30},
+]
+var source = $("#entry-template").html();
+var template = Handlebars.compile(source);
+var parentDiv = $("#calen");
+
+for (var i = 0; i<complexData.length; i++) {
+  var curData = complexData[i];
+  var curHtml = template(curData);
+  parentDiv.append(curHtml);
+}
+
+});
